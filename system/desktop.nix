cac64b756418c43d31e9d2e7368ceb4e31bab027
Jerdle-code <danielamdurer@gmail.com>
2024-11-25 18:22:00 +0000

    Here have code

--- system/desktop.nix
+++ system/desktop.nix
@@ -6,7 +6,7 @@
   services.xserver.videoDrivers = [ "amdgpu" ];
   services.xserver.displayManager.lightdm = {
   enable = true;
-  greeters.enso.enable = true;
+  greeters.slick.enable = true;
   };
 hardware.graphics.extraPackages = with pkgs; [
   rocmPackages.clr.icd
