17448f3d4d092208b9ec3e139331481b5f1b519f
Jerdle-code <danielamdurer@gmail.com>
2025-02-19 21:31:15 +0000

    Deleted Azorius - Will be using Proxmox there

--- flake.nix
+++ flake.nix
@@ -10,8 +10,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
-    portainer-on-nixos.url = "gitlab:cbleslie/portainer-on-nixos";
-    portainer-on-nixos.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, home-manager, plasma-manager, portainer-on-nixos, ... }@inputs: {
@@ -32,40 +30,6 @@
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
           }
-      ];
-    };
-    nixosConfigurations.Azorius = nixpkgs.lib.nixosSystem rec {
-      system = "x86_64-linux";
-      modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        Azorius/system/default.nix
-        Azorius/daniel/programs/default.nix
-        home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.daniel = import Azorius/daniel/default.nix;
-            home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
-          }
-        portainer-on-nixos.nixosModules.portainer
-        {
-          services.portainer = {
-            enable = true; # Default false
-
-            version = "latest"; # Default latest, you can check dockerhub for
-                                # other tags.
-
-            openFirewall = true; # Default false, set to 'true' if you want
-                                    # to be able to access via the port on
-                                    # something other than localhost.
-
-            port = 9443; # Sets the port number in both the firewall and
-                         # the docker container port mapping itself.
-          };
-        }
       ];
     };
   };
