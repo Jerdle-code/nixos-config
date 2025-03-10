dc8477500a478fa0439b8a7f19c89de9bd465300
Jerdle-code <danielamdurer@gmail.com>
2025-03-10 07:38:04 +0000

    Switch to systemd-boot in preparation for secure boot

--- Boros/system/boot.nix
+++ Boros/system/boot.nix
@@ -1,13 +1,13 @@
 { config, pkgs,... }:
 {
   # Bootloader.
-#boot.loader.systemd-boot.enable = true;
-boot.loader.grub = {
-enable = true;
-efiSupport = true;
-device = "nodev";
-useOSProber = true;
-};
+boot.loader.systemd-boot.enable = true;
+#boot.loader.grub = {
+#enable = true;
+#efiSupport = true;
+#device = "nodev";
+#useOSProber = true;
+#};
   boot.loader.efi.canTouchEfiVariables = true;
 boot.loader.efi.efiSysMountPoint = "/boot/efi";
 boot.kernel.sysctl = {"net.ipv4.ip_forward" = 1;};
