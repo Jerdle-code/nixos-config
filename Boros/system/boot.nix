df545a19c9af5ac80e7092994ca82a27934e9a3e
Jerdle-code <danielamdurer@gmail.com>
2025-04-13 20:07:16 +0100

    Oops

--- Boros/system/boot.nix
+++ Boros/system/boot.nix
@@ -10,6 +10,6 @@ boot.loader.systemd-boot.enable = true;
 #};
   boot.loader.efi.canTouchEfiVariables = true;
 boot.loader.efi.efiSysMountPoint = "/boot/efi";
-boot.kernel.sysctl = {"net.ipv4.ip_forward" = 1; "vm.swappiness" = 10;};
+boot.kernel.sysctl = {"net.ipv4.ip_forward" = 1;};
 boot.kernelPackages = pkgs.linuxPackages_latest;
 }
