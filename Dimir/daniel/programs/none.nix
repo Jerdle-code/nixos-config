b3b2329a0c7609cf6037d19d90dbafa84ec1f696
Jerdle-code <danielamdurer@gmail.com>
2025-06-05 18:03:09 +0100

    Fix the bugfux

--- Dimir/daniel/programs/none.nix
+++ Dimir/daniel/programs/none.nix
@@ -43,7 +43,7 @@ imports = [
       ]);
     home-manager.users.daniel.qt = {
       enable = true;
-      platformTheme.name = mkDefault "qt6ct";
+      platformTheme.name = lib.mkDefault "qt6ct";
     };
   };
 }
