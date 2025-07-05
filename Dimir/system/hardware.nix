a1bd86e8db0e470a36bb442ef193e8adb2bdf663
Jerdle-code <danielamdurer@gmail.com>
2025-07-05 17:54:42 +0100

    Add ryzen_smu

--- Dimir/system/hardware.nix
+++ Dimir/system/hardware.nix
@@ -71,4 +71,5 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.ryzen-smu.enable = true;
 }
