f0769728dd64c6d1147ceb352ea7ab46636c97ed
Jerdle <danielamdurer@gmail.com>
2025-10-02 23:07:49 +0100

    Increased Nix buffer size

--- Dimir/system/nix.nix
+++ Dimir/system/nix.nix
@@ -2,5 +2,6 @@
 {
   nix.settings.auto-optimise-store = true;
   nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings.download-buffer-size = 4294967296;
   system.stateVersion = "23.11";
 }
