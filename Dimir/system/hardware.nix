ef09f7d7ba0df23ff634ef8d4178ab6abd46a252
Jerdle <danielamdurer@gmail.com>
2026-05-05 23:54:46 +0100

    Add v4l utils and v4l2 loopback

--- Dimir/system/hardware.nix
+++ Dimir/system/hardware.nix
@@ -73,5 +73,5 @@
   boot.extraModprobeConfig = ''
     options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
   '';
-  environment.systemPackages = [ config.boot.kernelPackages.v4l2loopback.bin ];
+  environment.systemPackages = [ config.boot.kernelPackages.v4l2loopback.bin pkgs.v4l-utils ];
 }
