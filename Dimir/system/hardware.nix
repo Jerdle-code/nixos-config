c090058a506014c876513da29021270d86ee4a71
Jerdle <danielamdurer@gmail.com>
2026-05-05 23:52:40 +0100

    Test

--- Dimir/system/hardware.nix
+++ Dimir/system/hardware.nix
@@ -68,9 +68,10 @@
   };
   services.hardware.bolt.enable = true;
   boot.extraModulePackages = with config.boot.kernelPackages; [
-    v4l2loopback
+    v4l2loopback.out
   ];
   boot.extraModprobeConfig = ''
     options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
   '';
+  environment.systemPackages = [ config.boot.kernelPackages.v4l2loopback.bin ];
 }
