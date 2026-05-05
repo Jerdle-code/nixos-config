c3d07717fbe9df8f74b663d6347ad18773324233
Jerdle <danielamdurer@gmail.com>
2026-05-05 22:19:26 +0100

    Enable v4l2loopback

--- Dimir/system/hardware.nix
+++ Dimir/system/hardware.nix
@@ -4,7 +4,7 @@
 {
   config,
   lib,
-  #pkgs,
+  pkgs,
   modulesPath,
   pkgs-unstable,
   ...
@@ -68,4 +68,10 @@
     };
   };
   services.hardware.bolt.enable = true;
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
 }
